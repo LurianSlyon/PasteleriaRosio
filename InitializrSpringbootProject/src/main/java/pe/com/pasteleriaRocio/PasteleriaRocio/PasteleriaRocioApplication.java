16495b991ed81439bd4fe1b1f8f5eca52aede254
LurianSlyon <rafael3416@gmail.com>
2022-11-11 13:35:57 -0500

    Subi

---
+++ InitializrSpringbootProject/src/main/java/pe/com/pasteleriaRocio/PasteleriaRocio/PasteleriaRocioApplication.java
@@ -0,0 +1,13 @@
+package pe.com.pasteleriaRocio.PasteleriaRocio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteleriaRocioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasteleriaRocioApplication.class, args);
+	}
+
+}
---
+++ InitializrSpringbootProject/src/test/java/pe/com/pasteleriaRocio/PasteleriaRocio/PasteleriaRocioApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.pasteleriaRocio.PasteleriaRocio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteleriaRocioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
